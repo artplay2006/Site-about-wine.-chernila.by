c36b0af1e83b8ae80ad0051724516be86a18f8fa
artplay2006 <artemlavdar@gmail.com>
2024-11-25 14:19:35 +0300

    Добавьте файлы проекта.

---
+++ ASPFirst/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ASPFirst.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
